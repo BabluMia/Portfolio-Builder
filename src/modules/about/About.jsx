235a6f8c3fa8e735f7ee7055a08c7e117d416a25
rak1b <rak13.dev@gmail.com>
2022-11-24 20:28:57 +0600

    about and services

---
+++ src/modules/about/About.jsx
@@ -0,0 +1,69 @@
+import rk from "./img/rk.jpg";
+import rkr from "./img/rkr.png";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+import "./css/socialmedia.css";
+import "font-awesome/css/font-awesome.min.css";
+import { Link } from "react-router-dom";
+const About = () => {
+  return (
+    <>
+      <section id="about" className="full_screen">
+        <div className="about_container">
+          <div className="content_container">
+            <div className="about_content">
+              <h2>
+                <span>01.</span>About
+              </h2>
+              <br />
+
+              <p>
+                Hello! I'm Rakib,a full stack developer ,Currently doing an internship as a Django Developer. completed BSC in{" "}
+                <span>Daffodl International University.</span>
+                <br />
+                <br />
+                I love making stuff for the internet, whether it's website,blogs, apps, or something in between. My aim is to create products that are
+                user friendly,easy to use and perfect.
+                <br />
+                Here are a few technologies I've been working with recently:
+                <ul className="about_ul">
+                  <li>
+                    <span>
+                      <ArrowRightIcon className="li_arrow" />
+                    </span>
+                    Python
+                  </li>
+                  <li>
+                    <span>
+                      <ArrowRightIcon className="li_arrow" />
+                    </span>
+                    Django
+                  </li>
+                  <li>
+                    <span>
+                      <ArrowRightIcon className="li_arrow" />
+                    </span>
+                    JavaScript (ES6+)
+                  </li>
+                  <li>
+                    <span>
+                      <ArrowRightIcon className="li_arrow" />
+                    </span>
+                    React
+                  </li>
+                  <li>
+                    <span>
+                      <ArrowRightIcon className="li_arrow" />
+                    </span>
+                    And Obviously HTML,CSS
+                  </li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
---
+++ src/modules/services/Services.jsx
@@ -0,0 +1,32 @@
+import ServiceCard from "./Sub_component/ServiceCard";
+import ServiceData from "./Sub_component/ServiceData";
+import RKLogo from "./img/LogoRak1b.png";
+
+const Services = () => {
+  return (
+    <>
+      <section id="services" className="full_screen">
+        <div className="service_header">
+          <h2>
+            <span>03.</span>Services
+          </h2>
+        </div>
+        <div class="service_container">
+          <div className="service_cards_container">
+            {ServiceData.map((val, index) => {
+              return (
+                <ServiceCard
+                  image={val.image}
+                  header={val.header}
+                  desc={val.desc}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Services;
