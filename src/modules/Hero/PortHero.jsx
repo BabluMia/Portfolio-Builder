6dd4ae58be4fc51b2b40dfc8785c61ec0c3e7416
rak1b <rak13.dev@gmail.com>
2022-11-24 19:10:23 +0600

    porthero and css

---
+++ src/modules/Hero/PortHero.jsx
@@ -0,0 +1,20 @@
+// import Card from "./Card";
+const PortHero = () => {
+  return (
+    <>
+      <div className="port_hero" id="home">
+        <div className="hero_left">
+          <h3>Hi,my name is</h3>
+          <h2>Md Rakibul Islam.</h2>
+          <h4>I create stuff for the Internet.</h4>
+          <p>I am a Fullstack Web Devoloper From Bangladesh.I use React + Django stack.Currently open for work 😀</p>
+          <a href="mailto:mailto:rak13.dev@gmail.com?subject=Me&body=Hello!" className="hello_btn">
+            Say Hello
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PortHero;
--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import MuiNav from '../NavUp/MuiNav'
-import NavbarUp from '../NavUp/Navbar'
+import React from "react";
+import PortHero from "../Hero/PortHero";
+import MuiNav from "../NavUp/MuiNav";
+import NavbarUp from "../NavUp/Navbar";
 
 const Home = () => {
   return (
     <div>
-     <MuiNav/>
+      <MuiNav />
+      <PortHero />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
